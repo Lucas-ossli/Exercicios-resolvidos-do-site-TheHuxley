4d42a970a375112f3b63c2831c43aab3faa748e3
Pocoyyoo <90059655+Pocoyyoo@users.noreply.github.com>
2021-11-27 06:39:06 -0300

    Add files via upload

---
+++ 1 - 3o ângulo do triângulo.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+main(){
+	int a, b, soma;
+	float subtracao;
+	
+	cin>>a;
+	cin>>b;
+	
+	soma= a+b;
+	subtracao= 180-soma ;
+	
+	printf("3o angulo=%.6f", subtracao);
+	
+	return 0;
+	
+	
+	
+	
+	
+}
