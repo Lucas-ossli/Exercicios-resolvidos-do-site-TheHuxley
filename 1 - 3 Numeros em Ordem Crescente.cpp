dc6367ea4864dce1d89502f7d57a2477f7f7d2ad
Pocoyyoo <90059655+Pocoyyoo@users.noreply.github.com>
2021-11-30 14:10:50 -0300

    Add files via upload

---
+++ 1 - 3 Numeros em Ordem Crescente.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+using namespace std;
+
+main(){
+    int a,b,c;
+
+    cin>>a>>b>>c;
+
+    
+ 
+   //a b c
+   //a c b
+    if(a<=b && a<=c){
+        if(b<=c){
+            cout<<a<<"\n"<<b<<"\n"<<c;
+        }
+        else{
+            cout<<a<<"\n"<<c<<"\n"<<b;
+        }
+    }
+    //b a c
+    //b c a
+    else if(b<=a && b<=c){
+        if(a<=c){
+            cout<<b<<"\n"<<a<<"\n"<<c;
+        }
+        else{
+            cout<<b<<"\n"<<c<<"\n"<<a;
+        }
+    }
+
+    //c a b
+   // c b a
+    else if(c<=b && c<=a){
+        if(a<=b){
+            cout<<c<<"\n"<<a<<"\n"<<b;
+        }
+        else{
+            cout<<c<<"\n"<<b<<"\n"<<a;
+        }
+    }
+ 
+ 
+    return 0;
+}
