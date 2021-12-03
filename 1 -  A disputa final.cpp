a6fb6078c0193d5cee1da5d364d8b23f26eff31a
Pocoyyoo <90059655+Pocoyyoo@users.noreply.github.com>
2021-12-03 05:27:08 -0300

    Add files via upload

---
+++ 1 -  A disputa final.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	
+	int a;
+	
+	cin>>a;
+	if(a==0){
+		
+		cout<<"\nrosa rosa verde rosa";
+		
+	}
+	else if(a==1){
+		
+		cout<<"\nverde rosa verde rosa";
+	}
+	else if(a==2){
+		
+		cout<<"\nverde verde verde rosa";
+		
+	}
+	else if(a==3){
+		
+		cout<<"\nrosa verde verde rosa";
+		
+	}
+	else if(a==4){
+		
+		cout<<"\nrosa rosa rosa verde";
+		
+	}
+	else if(a==5){
+		cout<<"\nverde rosa rosa verde";
+		
+	}
+	else if(a==6){
+		cout<<"\nverde verde rosa verde";
+		
+	}
+	else if(a==7){
+		
+		cout<<"\nrosa verde rosa verde";
+		
+	}
+	else if(a==8){
+		
+		cout<<"\nverde verde verde verde";
+		
+	}
+	else if(a==9){
+		cout<<"\nrosa rosa rosa rosa";
+		
+	}
+	else if(a==10){
+		
+		cout<<"\nrosa rosa verde verde";
+		
+	}
+	else if(a==11){
+		
+		cout<<"\nverde verde rosa rosa";
+	}
+	
+	return 0;
+}
