0a833d23eb970eb9fd192bdfbc49d9ee7ddff6f5
Pocoyyoo <90059655+Pocoyyoo@users.noreply.github.com>
2021-12-04 12:19:37 -0300

    Add files via upload

---
+++ 2- A nave de Bilu.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+main(){
+	
+	//Link do exercício: https://www.thehuxley.com/problem/3547?locale=pt_BR
+	
+	int a,b,c,d;
+	cin>>a>>b>>c>>d;
+	
+	if(a*b<c*d){
+		cout<<"Pousa, bilu";
+	}
+	else
+		cout<<"Procura outro lugar, bilu";
+	return 0;
+}
